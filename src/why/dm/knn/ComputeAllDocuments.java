fb77f72a37e15f645f884e7fbd4a6b937d0f5e6d
qinhui_wang <qaddafi2008@126.com>
2012-03-23 23:20:28 +0800

    delete one delete

--- src/why/dm/knn/ComputeAllDocuments.java
+++ src/why/dm/knn/ComputeAllDocuments.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-import sun.font.FontConfigManager.FcCompFont;
 import why.dm.Classifier;
 import why.dm.Document;
 import why.dm.Feature;
